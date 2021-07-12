a11344cd812d8585ab33dadde54be64f579f907f
Matin <mghavami@berkeley.edu>
2021-07-12 11:00:00 -0700

    Fix typo

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Test
 
 @testset "LambdaCalculus.jl" begin
     include("terms_tests.jl")
-    include("debrujin_tests.jl")
+    include("debruijn_tests.jl")
     include("calculus_tests.jl")
     include("io_tests.jl")
 end
