5f1331218c910895d0ca72e0c591dd8b9f1808ee
Matin <mghavami@berkeley.edu>
2021-07-21 13:20:01 -0700

    export GLOBAL_CONTEXT

--- src/LambdaCalculus.jl
+++ src/LambdaCalculus.jl
@@ -5,7 +5,7 @@ export alpha_equivalent, is_beta_redex, beta_reduce, is_eta_redex, eta_reduce,
        normalize
 
 # term and De Bruijn-indexed accessors
-export UNTYPED, source, target, name, type, context, var, body,
+export UNTYPED, source, target, name, type, context, var, body, GLOBAL_CONTEXT,
        LambdaTypeError, source_type, operator, operand, free_vars, bound_vars,
        all_vars, debruijn_to_named, named_to_debruijn
 
